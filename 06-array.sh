898d913bd2c519b4f732dd69ed6e3b821d614583
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-11 14:09:44 +0530

    passing through cline

--- 06-array.sh
+++ 06-array.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 fruits=("banana" "grapes" "apple")
-echo "first fruitis $fruits[1]"
+echo "first fruitis ${fruits[1]}"
+echo "second fruit is ${fruits[2]}"
+echo "third fruit is ${fruits[3]}"
