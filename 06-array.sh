d3746a33183b42628de0d6cf6fec75d5dff94276
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-11 14:11:54 +0530

    array

--- 06-array.sh
+++ 06-array.sh
@@ -2,4 +2,5 @@
 fruits=("banana" "grapes" "apple")
 echo "first fruitis ${fruits[1]}"
 echo "second fruit is ${fruits[2]}"
-echo "third fruit is ${fruits[0]}"
+echo "third fruit is ${fruits[0]}"  
+echo "all fruits are ${fruits[@]}"
