69f45c542499ff8f720337e6803124b5c85a241c
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-12 13:21:25 +0530

    shell

--- 11-installation.sh
+++ 11-installation.sh
@@ -6,8 +6,13 @@ if [ $userid -ne 0 ]
 then 
     echo "please run the command through root privilages"
     exit 1
-else
-    echo "run the command"
 fi
 
-dnf install mysql
+dnf installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed going to install it"
+else
+    echo "mysql is already installed nothing to do"
+fi
