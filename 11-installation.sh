b946c6e231a52c97e1ae9714d6b9c1ba2ead73c0
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-12 12:58:22 +0530

    shell

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 userid=$(id -u)
 echo "print userid $userid"
+
+if [ userid -ne 0 ]
+then 
+    echo "please run the command through root privilages"
+else
+    echo "run the command"
+fi
+
+dnf install mysql
