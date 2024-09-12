d14545b06ac950e494c876c0bae1b347c41b5b13
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-12 13:48:18 +0530

    shell

--- 11-installation.sh
+++ 11-installation.sh
@@ -13,13 +13,31 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then 
     echo "mysql is not installed going to install it"
-    dnf install mysql
+    dnf install mysql -y
     if [$? -ne 0]
     then
         echo "mysql is not installed "
+        exit 1
     else
         echo "it is  installed "
     fi
 else
     echo "mysql is already installed nothing to do"
 fi
+
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed going to install it" 
+    dnf install nginx -y
+    if [ $? -ne 0]
+    then 
+        echo "nginx is not installed yet "
+        exit 1
+    else
+        echo "nginx is installed"
+    fi
+else 
+    echo "nginx is already installed"
