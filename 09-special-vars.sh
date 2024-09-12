b457aff2c6007ec5fa0c0c75f23261e72a6c7b1b
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-12 12:08:13 +0530

    shell

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -5,5 +5,5 @@ echo "name of the script :$0"
 echo "current working directory :$PWD"
 echo "home directory of user :$HOME"
 echo "process instant id of the script executing now: $$"
-sleep 100
+sleep 100&
 echo "pid of bg running process:$!"
