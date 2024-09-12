56be1bb88fa4f9a50ff05605b5a884ea67827d51
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-12 14:18:36 +0530

    shell

---
+++ 12-function.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then 
+    echo "please run it through root privilages"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "mysql is not installed "
+        exit 1
+    else 
+        echo "mysql is installed"
+}
+
+dnf list installed mysql
+
+
+
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed going to install it"
+    dnf install mysql -y
+    VALIDATE() $? 
+else 
+    echo "mysql is already installed"
+fi
+
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed going to install it"
+    dnf install nginx -y
+    VALIDATE() $?
+else
+    echo "nginx is already installed"
