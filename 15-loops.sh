4c417cdb089a0bc1d6d5f7a6f04c874bf46bd880
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-13 12:56:30 +0530

    shell

--- 15-loops.sh
+++ 15-loops.sh
@@ -2,4 +2,33 @@
 
 userid=$(id -u)
 
-echo "$userid"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    echo "$userid"
+    if [ $userid -ne 0 ]
+    then
+        echo "please run through root privilages" 
+        exit 1
+    fi
+}
+
+CHECK_ROOT
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo "Installing is failed"
+        exit 1
+    else
+        echo "Installing is success"
+    fi
+}
+
+for package in $@
+do
+ echo "package"
+done
+
