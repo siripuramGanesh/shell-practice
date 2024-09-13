325fb1c27af809c552029a0143d5d0823d415a1a
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-13 20:36:41 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -4,7 +4,6 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
 R="\e[31m"
