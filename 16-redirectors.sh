d3136416158e9333fef84d6b5f97182d161c9adc
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-13 21:18:06 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -31,7 +31,7 @@ VALIDATE(){
 }
 
 USAGE(){
-    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ...."
     exit 1
 }
 
