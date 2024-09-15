a0f4e5699c7e2def1eb0af3fcb9ab21e948ea50e
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-15 21:32:17 +0530

    shellpractice

--- 17-set.sh
+++ 17-set.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+#set -e is to stop the execution when there is any error at a specific line
+
+failure(){
+    echo "failed at $1:$2"
+}
+trap  'failure "${LINENO}" "$BASH_COMMAND"' ERR
+
 echo "hello world"
 echoooo "helloooooo"
 echo "world hello"
