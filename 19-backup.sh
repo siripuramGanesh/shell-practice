5dc7ae9328cba76f9cca9ed1247395577b2332c5
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-09-17 13:51:14 +0530

    shellscripting

--- 19-backup.sh
+++ 19-backup.sh
@@ -11,3 +11,25 @@ USAGE(){
  then 
     USAGE
 fi
+
+if [ ! -d $SOURCE_FILE ]
+then
+    echo "$SOURCE_FILE doesnot exist please check "
+    exit 1
+fi
+
+if [ ! -d $DEST_FILE ]
+then 
+    echo "$DEST_FILE doesnot exist please check "
+    exit 1
+fi
+
+FILES=$(find $SOURCE_FILE -name "*.log" -mtime +14)
+
+if [ ! -z $FILES ]
+then    
+    echo "files are found"
+else    
+    echo "files are not found"
+fi
+
